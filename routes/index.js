e0f242bed039cb95b042d66547869a9156cb0b55
rjforty <raaj.ahab@gmail.com>
2019-03-02 23:15:14 +0000

    Docker ignore file

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,7 @@
+const otpController = require("../controllers/otp");
+const otpSchema = require("../schema/otp");
+
+
 module.exports =  [
     {
         method: "GET",
@@ -5,5 +9,12 @@ module.exports =  [
         handler: async () => {
             return "Welcome to Polyottis"
         }
+    },
+
+    {
+        method: "POST",
+        url: "/api/v1/otp/create",
+        handler: otpController.createOtp,
+        schema: otpSchema.createOtpSchema
     }
 ];
